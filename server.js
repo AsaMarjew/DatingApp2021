b22f81947fe58356e248728bc3be223eaacf78a1
AsaMarjew <asa@marjew.nl>
2021-04-26 18:31:01 +0200

    npm EJS intalled and added dynamic webpage

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Asa Marjew
+
+// Loading the things we need
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -5,9 +8,48 @@ const port = 3000
 
 app.use(express.static('static'))
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+// index page 
+app.get('/', function(req, res) {
+    var matches = [
+        { name: 'Christof & Sammy', breed: "Pug", age: 35},
+        { name: 'Evan & Tux', breed: "Golden retriever", age: 14},
+        { name: 'Rosa & Moby Dock', breed: "Berner sennen", age: 25}
+    ];
+    var tagline = "No match is complete without your bestfriend at your side.";
+
+    res.render('pages/index', {
+        matches: matches,
+        tagline: tagline
+    });
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+/*// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+*/
+
+/*// Send file to server
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/pages/index.html'));
+    res.sendFile(path.join(__dirname + '/pages/login.html'));
 })
+
+*/
 
 app.get('/', function (req, res) {
     throw new Error('404 Not Found') // Express will catch this on its own.
