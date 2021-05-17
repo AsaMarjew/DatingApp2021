602b9c5a744c386157d86ac02e6526595655a348
AsaMarjew <asa@marjew.nl>
2021-05-17 09:39:06 +0200

    end of the day

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static('static'))
+app.use(express.static("public"));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
