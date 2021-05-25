ed8770fd0a6bad0689137d60b00fbca33a0275b9
AsaMarjew <asa@marjew.nl>
2021-05-25 16:09:36 +0200

    Mongodb connection

--- server.js
+++ server.js
@@ -1,87 +1,90 @@
-// Asa Marjew
+// Asa Marjew Tech 6
 
 // Loading the things we need
-const express = require('express')
-const path = require('path')
-const app = express()
-const mongoose = require("mongoose");
+const express = require('express');
+const app = express();
 const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended: true}));
+const dotenv = require("dotenv");
+dotenv.config()
 const port = 3000
+
+
+// connection with Mongodb
+const MongoClient = require('mongodb').MongoClient;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@datingapp2021.pdjok.mongodb.net/DatingApp2021?retryWrites=true&w=majority`;
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Connected to MongoDB!");
+    db.close();
+  });
+
+
+
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
 
 // public folder location
 app.use(express.static("public"));
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+// define routes
+app.get("/index", pageRegistration);
+app.get("/about", pageAbout);
+app.get("/results", pageResults);
+app.get("*", error);
+
+// Registration page render
+function pageRegistration(req, res) {
+    res.render('pages/index', {
+        matches: matches
+    });
+}
+
+// About page render
+function pageAbout(req, res) {
+    res.render('pages/about');
+}
+
+// Results page render
+function pageResults(req, res) {
+    res.render('pages/results');
+}
+
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/index', function(req, res) {
+    var item = {
+        fullName: req.body.fullName,
+
+    }
+    console.log(req.body);
+    matches.push(req.body);
+    console.log(matches);
+
+    Data.push({
+        fullName: req.body.fullName
+    })
+
+    res.render('pages/profileview');
+});
+
 
 var matches = [
     { fullName: "Rosa Matisse", accountName: "Moby Dick", password: "Moby-dick-is-cool", birthdate: "24/02/1995", gender: "female", 
     placeResidence: "Amsterdam", knowledge: "Amature", about: "I like colorfull art", location: "Noord-Holland", 
     category: "Art", goal: "Knowledge"},
-    { fullName: 'Luuk Dali', accountName: "Moby Dick", password: "Moby-dick-is-cool", birthdate: "24/02/1995", gender: "female", 
-    placeResidence: "Amsterdam", knowledge: "Amature", about: "I like colorfull art", location: "Noord-Holland", 
-    category: "Art", goal: "Knowledge"},
-    { fullName: 'Chris Darwin', accountName: "Moby Dick", password: "Moy-dick-is-cool", birthdate: "24/02/1995", gender: "female", 
-    placeResidence: "Amsterdam", knowledge: "Amature", about: "I like colorfull art", location: "Noord-Holland", 
-    category: "Art", goal: "Knowledge"}
 ];
 
-app.post('/index', function(req, res) {
-    console.log(req.body);
-    matches.push(req.body);
-    console.log(matches);
-    res.render('pages/results');
-});
-
-// index page with data
-app.get('/index', function(req, res) {
-
-    var tagline = "No match is complete without your bestfriend at your side.";
-
-    res.render('pages/index', {
-        matches: matches,
-        tagline: tagline
-    });
-});
-
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
-
-// results page
-app.get('/results', function(req, res) {
-    res.render('pages/results');
-});
-
-/*// index page
-app.get('/', function(req, res) {
-    res.render('pages/index');
-});
-
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
-*/
-
-/*// Send file to server
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/pages/login.html'));
-})
-
-*/
 
 
-// error
-app.use(function(req, res, next){
-    res.status(404);
-  // default to plain-text. send()
-    res.type('txt').send('404 NOT FOUND...');
-  });
+// error page
+function error(req, res) {
+    res.status(404).render("404");
+  };
 
 // port of server
 app.listen(port, () => {
