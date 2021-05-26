27c3a51fa0a8a3e816f2beb28ee83b140aff0b50
AsaMarjew <asa@marjew.nl>
2021-05-26 23:43:49 +0200

    Fixed the route problem and disabled the 404 page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config()
 const port = 3000
 
 
-// connection with Mongodb
+// Connection with Mongodb
 const MongoClient = require('mongodb').MongoClient;
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@datingapp2021.pdjok.mongodb.net/DatingApp2021?retryWrites=true&w=majority`;
 
@@ -19,17 +19,18 @@ MongoClient.connect(url, function(err, db) {
     db.close();
   });
 
-// set the view engine to ejs
+// Set the view engine to ejs
 app.set("view engine", "ejs");
 
-// public folder location
+// Public folder location
 app.use(express.static("public"));
 
-// define routes
+// Define routes
 app.get("/index", pageRegistration);
 app.get("/about", pageAbout);
 app.get("/results", pageResults);
-app.get("*", error);
+app.get("/profileview'", pageProfileview);
+//app.get("*", error);
 
 // Registration page render
 function pageRegistration(req, res) {
@@ -46,13 +47,16 @@ function pageResults(req, res) {
     res.render('pages/results');
 }
 
+// Profileview page render
+function pageProfileview(req, res) {
+    res.render('pages/profileview');
+}
 
-
-
+// Makes is possible to get form information from another body
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/index', function(req, res) {
-
+// Write the form data in the mongodb database
+app.post('/profileview', function(req, res) {
     var item = 
         { fullName: req.body.fullName, accountName: req.body.accountName, password: req.body.password, birthdate: req.body.birthdate, gender: req.body.gender, 
         placeResidence: req.body.placeResidence, knowledge: req.body.knowledge, about: req.body.about, location: req.body.location, 
@@ -61,10 +65,9 @@ app.post('/index', function(req, res) {
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
             var database = db.db("DatingApp2021");
-            database.collection("users").insertOne(item, function(err, res) {
+            database.collection("user").insertOne(item, function(err, res) {
               if (err) throw err;
               console.log("1 document inserted");
-            //   res.status(200).json("Mongo succes")
               db.close();
             });
           });
@@ -72,13 +75,37 @@ app.post('/index', function(req, res) {
     res.render('pages/profileview');
 });
 
-// res.status(200).json("kjbhkjhkj");
-// {res.status(400).json("stop");
+app.get('/test', function routeHandler(req, res) {
+    res.send('ok');
+});
+
+app.get('/profileview', function routeHandeler(req, res) { 
+ 
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var database = db.db("DatingApp2021");
+        //Find the first document  in the users collection:
+        database.collection("user").findOne({}, function(err, result) {
+          if (err) throw err;
+          res.send(result);
+
+          const user = result;
+          res.render('pages/profileview', {
+            user: user
+          });
+
+          db.close();
+        });
+      });
+})
+
+// res.status(200).json("continue");
+// res.status(400).json("stop");
 
 // error page
-function error(req, res) {
-    res.status(404).render('pages/404');
-  };
+// function error(req, res) {
+//     res.status(404).render('pages/404');
+//   };
 
 // port of server
 app.listen(port, () => {
