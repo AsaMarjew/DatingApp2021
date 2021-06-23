c5d13fe62b0d0ff2d8615aac4f1bc524c6596c9c
AsaMarjew <asa@marjew.nl>
2021-06-23 23:19:37 +0200

    user let changed to const

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 3000;
-let user;
+const user;
 
 // Connection with Mongodb
 const MongoClient = require('mongodb').MongoClient;
